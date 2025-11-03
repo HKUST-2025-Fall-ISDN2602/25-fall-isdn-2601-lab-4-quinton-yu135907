5c89fcde6f1306e1499d209a39d516073e9a8300
Quinton Yu <qchyu@connect.ust.hk>
2025-11-03 16:53:43 +0800

    Add files via upload

---
+++ Lab4_T1_Skeleton.ino
@@ -0,0 +1,30 @@
+/*Change all the ? in the code to Control the direction of rotation based on whether your selected pin is touched.*/
+
+#define IN1    23  // Replace the ? with the GPIO pin you selected to connect IN1
+#define IN2    22   // Replace the ? with the GPIO pin you selected to connect IN2
+#define TOUCH_PIN  14 // Replace the ? with the GPIO touch pin you selected 
+#define THRESHOLD  100  // Replace the ? with the reading when the pin is touched from Lab 3
+
+int touch_reading;
+
+void setup() {
+  pinMode(IN1, OUTPUT);  // Set IN1 as an output pin
+  pinMode(IN2, OUTPUT);  // Set IN2 as an output pin
+  
+  pinMode(TOUCH_PIN, INPUT); // Set TOUCH_PIN as an output pin
+}
+
+void loop() {
+  touch_reading = touchRead(TOUCH_PIN); // Read the touch sensor
+  
+  /* if TOUCH_PIN is touched, then rotate the shaft in clockwise
+     else TOUCH_PIN is not touched, then rotate the shaft in counterclockwise */
+  
+   if (touch_reading < THRESHOLD){
+     digitalWrite(IN1, HIGH);
+     digitalWrite(IN2, LOW);  
+   }else{
+     digitalWrite(IN1, LOW);
+     digitalWrite(IN2, HIGH);  
+   }
+}
